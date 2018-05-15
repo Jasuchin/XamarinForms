3641969856a762fd65a83cb47e532b491cfe1c96
Justine Grace S. Putot <justineputot@outlook.com>
2018-05-15 14:59:05 +0800

    Add project files.

---
+++ XamarinForms/XamarinForms/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinForms.Views;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace XamarinForms
+{
+	public partial class App : Application
+	{
+		
+		public App ()
+		{
+			InitializeComponent();
+
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            AppCenter.Start("android=c2c84262-b5da-43a7-9558-a719266c168e;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
